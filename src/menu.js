52d3a06ebe60ea37ea55a1b4773710e65a18a58e
Florian Kattner <florian.kattner@gmail.com>
2022-09-07 20:18:56 +0200

    Fill menu page with content

--- src/menu.js
+++ src/menu.js
@@ -6,9 +6,41 @@ const menu = function(){
     const main = createElement("main");
     const mainDiv = createElement("div", "content");
     const heroH2 = createElement("h2", "", "Our Menu");
+    mainDiv.appendChild(heroH2);
+
+    let cont = createElement("div", "item-wrapper");
+    let name = createElement("div", "", "Pizza Margeritha");
+    let price = createElement("div", "", "5€");
+    cont.appendChild(name);
+    cont.appendChild(price);
+    mainDiv.appendChild(cont);
+
+    name = createElement("div", "", "Pizza Diavola");
+    price = createElement("div", "", "5.50€");
+    cont.appendChild(name);
+    cont.appendChild(price);
+    mainDiv.appendChild(cont);
+
+    name = createElement("div", "", "Pizza Hawaii");
+    price = createElement("div", "", "6€");
+    cont.appendChild(name);
+    cont.appendChild(price);
+    mainDiv.appendChild(cont);
+
+    name = createElement("div", "", "Pizza Quattro Stagioni");
+    price = createElement("div", "", "6.50€");
+    cont.appendChild(name);
+    cont.appendChild(price);
+    mainDiv.appendChild(cont);
+
+    name = createElement("div", "", "Pizza Da Flo");
+    price = createElement("div", "", "6.50€");
+    cont.appendChild(name);
+    cont.appendChild(price);
+    mainDiv.appendChild(cont);
 
     main.appendChild(mainDiv);
-    mainDiv.appendChild(heroH2);
+    
 
     return main;
 }
